9d1ff21b22f33cf392b35fe64ba2ea30bca1bfd2
Ka Yan <kayanchiang1018@gmail.com>
2025-01-04 16:30:58 +0000

    added new page of AboutUs, added AboutUs  button on Navbar

--- frontend/src/pages/AboutUsPage.jsx
+++ frontend/src/pages/AboutUsPage.jsx
@@ -1,5 +1,5 @@
 import { Container, Heading, SimpleGrid, Text, Flex, HStack, VStack, Box, Image, Square, Button, StackDivider } from "@chakra-ui/react";
-import strawberry_cake from "/strawberry_cake.jpeg"
+import orange_flower from "/orange_flower.jpg"
 import { RiImageCircleLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import logo from "../images/main_logo.jpg";
@@ -10,7 +10,7 @@ import logo from "../images/main_logo.jpg";
 
 
 const HomePage = () => {
-	const logo_text = "All\n About\n Cake..."
+	const logo_text = "Why Kasumi?\n\nKasumi (霞草) translates to Gypsophila (滿天星), which is a type of flower.\n I am particularly fond of the flower language associated with orange gypsophila.\n\n The flower language for Orange Gypsophila (橙色滿天星花) symbolizes a joyful, blessing and encouraging sentiment filled with warmth and positivity.\n\n Kasumi's mission is to spread this goodness to every patron."
 	return (
 		<Container maxW='container.2xl' py={18} >
 			<Link to={"/"}>   
@@ -30,12 +30,12 @@ const HomePage = () => {
 			</Link>
 
 			<Flex pt='10' spacing={20} direction='row' >
-				<Square flex='1' bg='#F2EEE8' size={{ base: "250px", lg: "550px" }}>
-			       <Text fontSize={{ base: "30px", lg: "80px" }} whiteSpace="pre-line" as='em'>{logo_text}</Text>
-		        </Square>
 				<Square flex='2' bg='#F2EEE8' size={{ base: "250px", lg: "550px" }}>
+			       <Text fontSize={{ base: "10px", lg: "20px" }} whiteSpace="pre-line" as='em'>{logo_text}</Text>
+		        </Square>
+				<Square flex='1' bg='#F2EEE8' size={{ base: "250px", lg: "550px" }}>
 			       
-				   <Image  boxSize={{ base: "250px", lg: "550px" }} objectFit='cover'  src={strawberry_cake} alt="strawberry cake" />
+				   <Image  boxSize={{ base: "250px", lg: "550px" }} objectFit='cover'  src={orange_flower} alt="orange flower" />
 
 		        </Square>	   
 			</Flex>
@@ -44,12 +44,7 @@ const HomePage = () => {
 						divider={<StackDivider borderColor='gray.200' />}
 						align='center'
 						pt='50'>
-				   <Link to={"/menu"}>
-						<Button variant="outline" colorScheme='yellow' as='em' align={"center"}>  
-						♥︎Shop now
-							
-						</Button>
-					</Link>
+				  
 
 					</VStack>
 		</Container>
