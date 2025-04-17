f24ded5ce0a3445a7779506c5b0cf40d8c5422e8
Ka Yan <kayanchiang1018@gmail.com>
2025-04-17 11:55:47 +0100

    gallery and contact us page update

--- frontend/src/components/Slick/Slick.jsx
+++ frontend/src/components/Slick/Slick.jsx
@@ -8,6 +8,11 @@ import gallery_picture_7 from "/gallery_pictures/gallery_picture_7.jpg";
 import gallery_picture_8 from "/gallery_pictures/gallery_picture_8.jpg";
 import gallery_picture_9 from "/gallery_pictures/gallery_picture_9.jpg";
 import gallery_picture_10 from "/gallery_pictures/gallery_picture_10.jpg";
+import gallery_picture_11 from "/gallery_pictures/gallery_picture_11.jpg";
+import gallery_picture_12 from "/gallery_pictures/gallery_picture_12.jpg";
+import gallery_picture_13 from "/gallery_pictures/gallery_picture_13.jpg";
+import gallery_picture_14 from "/gallery_pictures/gallery_picture_14.jpg";
+import gallery_picture_15 from "/gallery_pictures/gallery_picture_15.jpg";
 import "./Slick.css";
 
 function Slick() {
@@ -33,7 +38,7 @@ function Slick() {
         <img src={gallery_picture_10} width="80%" />
       </div>
       <div>
-        <img src={gallery_picture_8} width="80%" />
+        <img src={gallery_picture_12} width="80%" />
       </div>
       <div>
         <img src={gallery_picture_1} width="80%" />
@@ -49,6 +54,18 @@ function Slick() {
       </div>
       <div>
         <img src={gallery_picture_3} width="80%" />
+      </div>
+      <div>
+        <img src={gallery_picture_11} width="80%" />
+      </div>
+      <div>
+        <img src={gallery_picture_13} width="80%" />
+      </div>
+      <div>
+        <img src={gallery_picture_14} width="80%" />
+      </div>
+      <div>
+        <img src={gallery_picture_15} width="80%" />
       </div>
     </Slider>
   );
